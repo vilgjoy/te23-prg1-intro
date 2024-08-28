4bbbceed9db95ba4ce05ec65d7b22ee14df37341
¨Vilgot <vilgot.sandberg@elev.ga.ntig.se>
2024-08-28 09:29:54 +0200

    variabler

---
+++ lektion-1.py
@@ -0,0 +1,5 @@
+namn = input("Skriv ditt namn: ")
+viktigt = "tecknet vi använder till stränger heter fnuttar. fnuttar är dessa vi''."
+print(f"{viktigt} och det ska {namn} komma ihåg!")
+
+print(type(namn))
